a8096f240ee0b93079d9b4a037c10b9378f9bc08
jackyyoyo2@gmail.com <jackyyoyo2@gmail.com>
2024-06-22 12:48:47 +0800

    q1 solution with C++

---
+++ CPP/leetcode_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+            mp[nums[i]] = i;
+        }
+        throw invalid_argument("No two sum solution");
+    }
+};
